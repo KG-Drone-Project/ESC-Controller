e6eca4e99ebdc3642d6088341d71a62e06f5994d
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-02-01 17:14:45 -0500

    Added map function

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use core::cell::{Cell, RefCell};
 use stm32f4xx_hal::{
-    pac::{self},
+    pac::{self, stk::val},
     prelude::*,
     timer::{Channel1, Channel2},
 };
@@ -36,21 +36,41 @@ fn main() -> ! {
 
     let pwm = dp.TIM1.pwm_hz(channels, 50.Hz(), &clocks).split();
     let (mut ch1, _ch2) = pwm;
-    let max_duty = ch1.get_max_duty();
+    let max_duty = ch1.get_max_duty() as u32; 
     ch1.enable();
 
     // Zero signal
     rprintln!("Zero signal");
-    ch1.set_duty(max_duty / 20);
+    ch1.set_duty(ch1.get_max_duty() / 20);
 
     rprintln!("Slight delay");
     delay.delay_ms(10000_u32);
 
-    rprintln!("Mid throttle signal");
-    ch1.set_duty(max_duty / 18);
+
+    // Minimum signal is 1 ms => 5%
+    // Max signal is 2 ms => 10%
+
+    let min_limit = max_duty * 5 / 1000;
+    let max_limit = max_duty / 100;
+
+    rprintln!("Test");
+    for value in 0..50 {
+        //ch1.set_duty(map(value, 0, 100, min_limit, max_limit));
+        let throttle = map(value, 0, 100, min_limit, max_limit);
+        ch1.set_duty(throttle as u16);
+        delay.delay_ms(12_u32);
+    }
+
     loop {
 
         //rprintln!("{:?}", ch1.get_max_duty());
 
     }
+
+        // Function that maps one range to another
+    fn map(x: u32, in_min: u32, in_max: u32, out_min: u32, out_max: u32) -> u32 {
+        (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    }
+
+
 }
